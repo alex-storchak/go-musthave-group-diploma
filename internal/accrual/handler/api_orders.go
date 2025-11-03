e175853ade0ea612f34c0859a6ba86197ce95619
Alexander Storchak <alexander.s.storchak@gmail.com>
2025-11-03 15:44:22 +0300

    Добавил тесты для хэндлера accrual для эндпоинта /api/orders

--- internal/accrual/handler/api_orders.go
+++ internal/accrual/handler/api_orders.go
@@ -13,6 +13,13 @@ import (
 	"strconv"
 )
 
+const (
+	MsgEmptyOrderNumber     = "order is required"
+	MsgInvalidOrderNumber   = "order is invalid (Luhn algorithm check failed)"
+	MsgEmptyGoodDescription = "description is required"
+	MsgInvalidGoodPrice     = "price must be greater than 0"
+)
+
 type reqGood struct {
 	Description string  `json:"description"`
 	Price       float64 `json:"price"`
@@ -27,19 +34,17 @@ func (o reqOrder) Valid() validator.Problems {
 	problems := make(validator.Problems)
 
 	if o.Number == "" {
-		problems["order"] = "order is required"
-	}
-
-	if !validator.IsValidLuhn(o.Number) {
-		problems["order"] = "order is invalid (Luhn algorithm check failed)"
+		problems["order"] = MsgEmptyOrderNumber
+	} else if !validator.IsValidLuhn(o.Number) {
+		problems["order"] = MsgInvalidOrderNumber
 	}
 
 	for i, g := range o.Goods {
 		if g.Description == "" {
-			problems["goods."+strconv.Itoa(i)+".description"] = "description is required"
+			problems["goods."+strconv.Itoa(i)+".description"] = MsgEmptyGoodDescription
 		}
 		if g.Price <= 0 {
-			problems["goods."+strconv.Itoa(i)+".price"] = "price must be greater than 0"
+			problems["goods."+strconv.Itoa(i)+".price"] = MsgInvalidGoodPrice
 		}
 	}
 
